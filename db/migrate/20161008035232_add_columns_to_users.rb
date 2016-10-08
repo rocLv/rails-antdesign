4ae621132c138b08150ff87286aa0e3c3d76ec87
吴要权 <yqwoe@live.cn>
2016-10-08 13:13:40 +0800

    修改

---
+++ db/migrate/20161008035232_add_columns_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :mobile, :string, null: false,unique: true
+    add_column :users, :realname, :string,unique: true
+    add_column :users, :username, :string,unique: true
+    add_column :users, :wx, :string,unique: true
+    add_column :users, :state, :Integer
+  end
+end
